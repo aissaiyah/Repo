0dbbaad37e2444ef842e4630b9171db6dc86d585
Bus <bookwormtwenty@gmail.com>
2024-02-17 21:52:59 -0500

    Final Movement

--- Assets/movementScript.cs
+++ Assets/movementScript.cs
@@ -4,55 +4,43 @@ using UnityEngine;
 
 public class movementScript : MonoBehaviour
 {
-    public bool canMove;
-    public BoxCollider2D bc;
-    private int[] movement = new int[2];
-    private bool move;
-
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        bc = this.GetComponent<BoxCollider2D>();
-    }
 
     // Update is called once per frame
     void Update()
     {
-        move = false;
 
         if (Input.GetKeyDown(KeyCode.D))
         {
-            move = true;
-            movement = new int[] { 1, 0 };
+            transform.Translate(1, 0, 0);
+            if (Physics2D.OverlapCircle(transform.position,.2f))
+            {
+                transform.Translate(-1, 0, 0);
+            }
+            
         }
         else if (Input.GetKeyDown(KeyCode.A))
         {
-            move = true;
-            movement = new int[] { -1, 0 };
+            transform.Translate(-1, 0, 0);
+            if (Physics2D.OverlapCircle(transform.position, .2f))
+            {
+                transform.Translate(1, 0, 0);
+            }
         }
         else if (Input.GetKeyDown(KeyCode.W))
         {
-            move = true;
-            movement = new int[] { 0, 1 };
+            transform.Translate(0, 1, 0);
+            if (Physics2D.OverlapCircle(transform.position, .2f))
+            {
+                transform.Translate(0, -1, 0);
+            }
         }
         else if (Input.GetKeyDown(KeyCode.S))
         {
-            move = true;
-            movement = new int[] { 0, -1 };
-        }
-
-        if(move) transform.Translate(movement[0], movement[1], 0);
-
-    }
-
-    void OnCollisionStay2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "wall")
-        {
-            transform.Translate(-1*movement[0], -1*movement[1], 0);
-            print(movement[0]+" " + movement[1]);
+            transform.Translate(0, -1, 0);
+            if (Physics2D.OverlapCircle(transform.position, .2f))
+            {
+                transform.Translate(0, 1, 0);
+            }
         }
     }
 }
