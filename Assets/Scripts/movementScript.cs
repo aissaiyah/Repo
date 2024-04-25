81aba50fee05bd81731ad91fbb37a82f61226257
Bus <bookwormtwenty@gmail.com>
2024-04-25 10:00:07 -0400

    Movement fix

--- Assets/Scripts/movementScript.cs
+++ Assets/Scripts/movementScript.cs
@@ -8,41 +8,39 @@ public class movementScript : MonoBehaviour
    
     void Update()
     {
-        int[] direction=new int[] {0,0};
-
-        if (Input.GetKeyDown(KeyCode.D))      direction = new int[] { 1, 0 };
+        int[] direction=new int[] {0,0};//direction the player moves {x,y}
+        if      (Input.GetKeyDown(KeyCode.D)) direction = new int[] { 1, 0 };
         else if (Input.GetKeyDown(KeyCode.A)) direction = new int[] { -1, 0 };
         else if (Input.GetKeyDown(KeyCode.W)) direction = new int[] { 0, 1 };
         else if (Input.GetKeyDown(KeyCode.S)) direction = new int[] { 0, -1 };
-        if(Input.GetKeyDown(KeyCode.R)) SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-
-        transform.Translate(direction[0], direction[1], 0);
-        Collider2D collision = Physics2D.OverlapCircle(transform.position, .2f);
-
-        if (collision != null)
+        if(Input.GetKeyDown(KeyCode.R)) SceneManager.LoadScene(SceneManager.GetActiveScene().name);//Restart level
+      
+        transform.Translate(direction[0], direction[1], 0);//move player
+        Collider2D[] collisionList = Physics2D.OverlapCircleAll(transform.position, .2f);//create a collision circle to check if they moves into something
+        
+        if (collisionList.Length > 0)//if there is an object overlapping with the collision circle
         {
-            if (collision.gameObject.tag == "wall" || collision.gameObject.tag == "player") transform.Translate(-1 * direction[0], -1 * direction[1], 0);
-            else if (collision.gameObject.tag == "laser") SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-            else if (collision.gameObject.tag == "box" || collision.gameObject.tag == "eggBox")
+            for (int i = 0; i < collisionList.Length; i++)
             {
-                collision.gameObject.transform.Translate(new Vector2(direction[0], direction[1]));
-                Collider2D collision2 = Physics2D.OverlapCircle(collision.gameObject.transform.position, .2f);
-                if (collision2 != null)
+                if (collisionList[i].gameObject.tag == "solid" || collisionList[i].gameObject.tag == "player") transform.Translate(-1 * direction[0], -1 * direction[1], 0);//if you collide with a wall or other player you move back to where you where
+                else if (collisionList[i].gameObject.tag == "laserBeam") SceneManager.LoadScene(SceneManager.GetActiveScene().name);//if you hit a laser you die and the level restarts
+                else if (collisionList[i].gameObject.tag == "box")//if its a box then you can push it
                 {
-                    if (collision2.gameObject.tag == "wall" || collision2.gameObject.tag == "player" || collision2.gameObject.tag == "box" || collision2.gameObject.tag == "eggBox")
+                    collisionList[i].gameObject.transform.Translate(new Vector2(direction[0], direction[1]));//move box
+                    Collider2D[] collisionList2 = Physics2D.OverlapCircleAll(collisionList[i].gameObject.transform.position, .2f);//check collision for box
+                    if (collisionList2.Length > 0)
                     {
-                        transform.Translate(-1 * direction[0], -1 * direction[1], 0);
-                        collision.gameObject.transform.Translate(new Vector2(-1 * direction[0], -1 * direction[1]));
-                    }
-                    else if(collision.gameObject.tag == "eggBox" && collision2.gameObject.tag == "laser")
-                    {
-                        collision.gameObject.GetComponent<CheckBoxScript>().Lasered = true;
-                        if(GameObject.Find("CheckBox (1)").GetComponent<CheckBoxScript>().Lasered == true
-                            && GameObject.Find("CheckBox (2)").GetComponent<CheckBoxScript>().Lasered == true) SceneManager.LoadSceneAsync("Win");
+                        for (int i2 = 0; i2 < collisionList.Length; i2++) 
+                        {
+                            if (collisionList2[i2].gameObject.tag == "solid" || collisionList2[i2].gameObject.tag == "player" || collisionList2[i2].gameObject.tag == "box")//if it hits something it and the player moves back
+                            {
+                                transform.Translate(-1 * direction[0], -1 * direction[1], 0);
+                                collisionList[i].gameObject.transform.Translate(new Vector2(-1 * direction[0], -1 * direction[1]));
+                            }
+                        }
                     }
                 }
             }
-
         }
     }
 }
