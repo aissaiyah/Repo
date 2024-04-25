e7fb103b56f4e4c312e48d9a0228a9fbc7092734
aissaiyah <aissaiyah.bennett.sia@gmail.com>
2024-04-25 13:38:58 -0400

    pushing temp changes

--- Assets/GMScript.cs
+++ Assets/GMScript.cs
@@ -19,7 +19,7 @@ public class GMScript : MonoBehaviour
         Collider2D[] collisionList = Physics2D.OverlapCircleAll(transform.position, .2f);
         for (int i = 0; i < collisionList.Length; i++)
         {
-            if (collisionList[i].gameObject.tag == "laserBeam") Lasered = true;
+            if (collisionList[i].gameObject.tag == "laser") Lasered = true;
         }
         if (GameObject.Find("CheckBox (1)").GetComponent<CheckBoxScript>().Lasered == true// if both boxes are lasered you win
         && GameObject.Find("CheckBox (2)").GetComponent<CheckBoxScript>().Lasered == true) SceneManager.LoadSceneAsync("Win");
